32f2c1e37b55c155e7bcf56d7e051eb4089f3b4c
iipavlova10-english <iipavlova10@gmail.com>
2026-03-18 11:55:28 +0300

    rebuild for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 
-// Конфиг Vite для GitHub Pages
 export default defineConfig({
-// root можно не указывать, по умолчанию '.'
-base: '/travelenglish/', // ВАЖНО: имя репозитория c учётом регистра
+  base: '/travelenglish/',
 });
